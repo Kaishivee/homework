a4f24de50b69b35a0405ac52065c7650b81920ad
Дарья <167187299+Kaishivee@users.noreply.github.com>
2024-04-22 13:40:05 +0300

    Add files via upload

---
+++ Списки и словари.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'peach', 'grape', 'orange', 'pineapple', 'banana', 'pear']
+print(my_list)
+print(my_list[0], (my_list[6])) #или (my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'coconut'
+print(my_list)
+
+my_dict = {'bear': 'медведь', 'house': 'дом', 'family': 'семья', 'friend': 'друг'}
+print(my_dict)
+my_dict['lime'] = 'лайм'
+print(my_dict)
